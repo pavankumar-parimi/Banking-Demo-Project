36475a283531f93915bb1488bbb7b806eeae7279
Venkat <081598pavankumar@gmail.com>
2020-07-20 00:03:24 +0530

    Completed after Login Functionality

--- Models/UserModel.py
+++ Models/UserModel.py
@@ -1,13 +1,19 @@
 from Database.ConnectionModel import ConnectionModel
+from datetime import datetime
 
 
 class UserModel(ConnectionModel):
-    collection_name = "user"
+    user_collection_name = "user"
+    user_trans_collection_name = "trans_details"
 
     def __init__(self):
-        self.user_collection = ConnectionModel.connection(UserModel.collection_name)
+        self.user_collection = ConnectionModel.connection(UserModel.user_collection_name)
+        self.user_trans_collection = ConnectionModel.connection(UserModel.user_trans_collection_name)
 
     def user_registration(self, email, password, mobile_no, role, amount):
+        cross_ref = self.user_collection.find({"email": email}).count()
+        if cross_ref:
+            return "There is already an user with that id"
         data = {
             "email": email,
             "password": password,
@@ -16,7 +22,10 @@ class UserModel(ConnectionModel):
             "amount": amount
         }
         response = self.user_collection.insert_one(data)
-        if response.inserted_id:
+        trans_data = {"user_id": self.user_collection.find_one({"email": email})["_id"], "debit_trans": [],
+                      "credit_trans": [{"cred_amount": amount, "time": datetime.utcnow()}]}
+        response_2 = self.user_trans_collection.insert_one(trans_data)
+        if response.inserted_id and response_2.inserted_id:
             return "Registered Successfully!!"
         else:
             return "Registration Failed!!"
@@ -28,6 +37,6 @@ class UserModel(ConnectionModel):
         }
         response = self.user_collection.find_one(query, {"role": 1})
         if response:
-            return response["role"], response["email"]
+            return response["role"], response["_id"]
         else:
             return 0
